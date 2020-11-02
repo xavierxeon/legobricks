12e4bf51bcf913cccedbcc39ed47a19db466ec1b
Ralf Waspe <rwaspe@me.com>
2020-11-02 23:10:06 +0100

    export stuff and prepare for force sensor

---
+++ legobricks/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from .brick import Brick, Firmware
+from .hub import Hub
+from .lightmatrix import LighMatrix
+from .mainloop import mainLoopConsole
+from .motor_pair import MotorPair
+from .motor_single import MotorSingle
+from .port import Port
+from .sensor_color import SensorColor
+from .sensor_distance import SensorDistance
+from .sensor_force import SensorForce
+from .speaker import Speaker
